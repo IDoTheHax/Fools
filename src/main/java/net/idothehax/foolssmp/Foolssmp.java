d4e02a38dc1eafc2addf83ae56de3a9e07603126
IDoTheHax <hax@idothehax.com>
2025-03-01 14:36:50 +0100

    Init Project

---
+++ src/main/java/net/idothehax/foolssmp/Foolssmp.java
@@ -0,0 +1,12 @@
+package net.idothehax.foolssmp;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Foolssmp implements ModInitializer {
+    public String MOD_ID = "foolssmp";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
