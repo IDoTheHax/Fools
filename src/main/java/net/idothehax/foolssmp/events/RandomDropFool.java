cefed8482192fdc86e4e58892f00460e8e01618e
IDoTheHax <hax@idothehax.com>
2025-03-01 15:51:12 +0100

    Ok now ive tweaked the drops

--- src/main/java/net/idothehax/foolssmp/events/RandomDropFool.java
+++ src/main/java/net/idothehax/foolssmp/events/RandomDropFool.java
@@ -3,19 +3,18 @@ package net.idothehax.foolssmp.events;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.text.Text;
 import net.minecraft.util.collection.DefaultedList;
 
 import java.util.Random;
 
 public class RandomDropFool {
     private static final Random random = new Random();
-    public static final int DROP_INTERVAL_TICKS = 1200; // Every minute (20 ticks * 60 seconds)
+    public static final int DROP_INTERVAL_TICKS = 600; // Every 30 seconds an item drops has a chance to drop
 
     public static void tickDrops(Iterable<ServerWorld> worlds) {
         for (ServerWorld world : worlds) {
             for (ServerPlayerEntity player : world.getPlayers()) {
-                if (random.nextInt(100) < 10) { // 10% chance to drop an item
+                if (random.nextInt(100) < 25) { // 25% chance for testing
                     dropRandomItem(player);
                 }
             }
