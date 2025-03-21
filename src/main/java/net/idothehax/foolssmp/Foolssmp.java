bb2508f5de140b262b83d5734c5a2eb925953722
IDoTheHax <hax@idothehax.com>
2025-03-21 17:13:54 +0100

    daily commit ig

--- src/main/java/net/idothehax/foolssmp/Foolssmp.java
+++ src/main/java/net/idothehax/foolssmp/Foolssmp.java
@@ -5,14 +5,22 @@ import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.idothehax.foolssmp.blocks.LuckyBlock;
 import net.idothehax.foolssmp.commands.FoolsCommands;
 import net.idothehax.foolssmp.events.RandomDropFool;
 import net.idothehax.foolssmp.events.SoundsFool;
 import net.idothehax.foolssmp.events.achievements.LavaDeathTracker;
 import net.idothehax.foolssmp.events.achievements.NuclearCreeper;
 import net.minecraft.advancement.AdvancementEntry;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.damage.DamageTypes;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +28,11 @@ import org.slf4j.LoggerFactory;
 public class Foolssmp implements ModInitializer {
     public static String MOD_ID = "foolssmp";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static final Block LUCKY_BLOCK = registerBlock("lucky_block", new LuckyBlock(AbstractBlock.Settings.create()
+            .strength(1.0F, 1200.0F)
+            .sounds(BlockSoundGroup.COPPER)
+            .emissiveLighting(Blocks::always)));
 
     @Override
     public void onInitialize() {
@@ -42,7 +55,7 @@ public class Foolssmp implements ModInitializer {
         });
 
         ServerLifecycleEvents.SERVER_STARTED.register(server -> {
-            Identifier achievementId = Identifier.of("foolssmp", "hot_tub_time_machine");
+            Identifier achievementId = Identifier.of("foolssmp", "advancement/hot_tub_time_machine");
             AdvancementEntry advancementEntry = server.getAdvancementLoader().get(achievementId);
             if (advancementEntry != null) {
                 LOGGER.info("Hot Tub Time Machine advancement loaded successfully");
@@ -55,4 +68,13 @@ public class Foolssmp implements ModInitializer {
 
         NuclearCreeper.register();
     }
+
+    private static <T extends Item> T registerItem(String id, T item) {
+        return Registry.register(Registries.ITEM, Identifier.of(MOD_ID, id), item);
+    }
+
+    private static <T extends Block> T registerBlock(String id, T block) {
+        return Registry.register(Registries.BLOCK, Identifier.of(MOD_ID, id), block);
+    }
+
 }
